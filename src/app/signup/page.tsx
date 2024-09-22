0a8dc7f119e5fcb93083b8ce20cd2a3b9606ce07
Ereur <amoussaouianas@gmail.com>
2024-09-22 21:46:12 +0100

    signup

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Reservations: React.FC = () => {
+  return (
+    <div className="w-full flex items-center justify-center h-full">
+      <p className="mt-[300px] font-bold text-2xl text-[#FF6F61]">
+        this section will work after we setup our SMTP
+      </p>
+    </div>
+  );
+};
+
+export default Reservations;
+
+// const UserPanel: React.FC = () => {
